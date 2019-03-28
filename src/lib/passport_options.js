0de8833df255b8a88963bdcf00f7de13c01683ad
Ghadeer Alkhathlan <g.alkhathlan@gmail.com>
2019-03-28 08:46:53 +0300

    first commit

---
+++ src/lib/passport_options.js
@@ -0,0 +1,18 @@
+// storing JWT as a cookie and using HTTPS for all web transactions.
+// never store JWT in LocalStorage malicious attackers will not be able to
+// steal our user’s JWT using XSS.
+import dotenv from "dotenv";
+dotenv.config();
+
+export const jwtOptions = {
+  jwtFromRequest: req => {
+    console.log("\n\n\n\nn -----", req.cookies);
+    return req.cookies.jwt;
+  },
+  secretOrKey: process.env.PASS_KEY
+};
+
+export const localOptions = {
+  usernameField: "email",
+  passwordField: "password"
+};
